afdcd9b820556fd2486d5e704bb4431a8be172ae
renxia <2962298936@qq.com>
2024-06-20 22:24:56 +0800

    export function demo

--- src/main/java/com/example/contactsapp_experimentalweek/SettingActivity.java
+++ src/main/java/com/example/contactsapp_experimentalweek/SettingActivity.java
@@ -1,23 +1,49 @@
 package com.example.contactsapp_experimentalweek;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.Settings;
 import android.view.MenuItem;
+import android.Manifest;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 public class SettingActivity extends AppCompatActivity {
+
+    private static final int REQUEST_CODE = 1024;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
 
-        Toolbar toolbar=findViewById(R.id.toolbar_configuration);
+        Toolbar toolbar = findViewById(R.id.toolbar_configuration);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowTitleEnabled(false); // 隐藏标题
+
+        // 导出按钮点击事件
+        Button exportButton = findViewById(R.id.button2);
+        exportButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                requestPermission();
+            }
+        });
     }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
@@ -30,13 +56,71 @@ public class SettingActivity extends AppCompatActivity {
                 return super.onOptionsItemSelected(item);
         }
     }
+
     //重写onBackPressed()方法
     @Override
     public void onBackPressed() {
-        Intent intent = new Intent(SettingActivity.this,MainActivity.class);
+        Intent intent = new Intent(SettingActivity.this, MainActivity.class);
         //创建一个新的任务栈，并且之前任务栈中的所有活动都会被清除
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
         startActivity(intent);
         super.onBackPressed();
+    }
+
+    private void requestPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            // Android 11及以上版本，使用新的存储权限管理方式
+            if (Environment.isExternalStorageManager()) {
+                writeFile();
+            } else {
+                // 请求用户授权管理所有文件访问权限
+                Intent intent = new Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION);
+                intent.setData(Uri.parse("package:" + getPackageName()));
+                startActivityForResult(intent, REQUEST_CODE);
+            }
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            // Android 6.0及以上版本，需要动态请求存储权限
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                writeFile();
+            } else {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_CODE);
+            }
+        } else {
+            // Android 6.0以下版本，直接写入文件
+            writeFile();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                writeFile();
+            } else {
+                Toast.makeText(this, "存储权限获取失败", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE && Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            // 在Android 11及以上版本，处理权限请求结果
+            if (Environment.isExternalStorageManager()) {
+                writeFile();
+            } else {
+                Toast.makeText(this, "存储权限获取失败", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    /**
+     * 模拟文件写入操作
+     */
+    private void writeFile() {
+        Toast.makeText(this, "写入文件成功", Toast.LENGTH_SHORT).show();
+        // 这里可以添加具体的文件写入逻辑
     }
 }
